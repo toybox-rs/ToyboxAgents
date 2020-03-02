5772098b29b92eb234c113c002be89da9a96de8f
Emma Tosch <etosch@cs.umass.edu>
2020-03-02 14:15:30 -0500

    updated to targeting agent

--- agents/breakout/stayalive.py
+++ agents/breakout/stayalive.py
@@ -94,12 +94,14 @@ class Target(StayAliveJitter):
             paddley = game.paddle.position.y
             paddle_width = game.paddle_width
 
-            is_close = abs(bally - paddley) <= 0.25 * paddle_width
+            is_close = abs(bally - paddley) < 0.2 * paddle_width
 
             if intervention.num_bricks_remaining() == intervention.num_bricks() or not is_close:
                 return super().get_action()
 
+
             # TARGETING TIME
+            print('TARGET TIME')
             # get the column with the fewest bricks greater than zero
             # if there is one brick, stop early
             target_col = None
@@ -111,7 +113,7 @@ class Target(StayAliveJitter):
 
                 # set the first column
                 if target_col is None:
-                    target_col = col
+                    target_col = this_col
                     num_target_col_alive = len([int(brick.alive) for brick in target_col])
                 # don't want the target col to be an already-formed channel
                 elif num_target_col_alive == 0:
