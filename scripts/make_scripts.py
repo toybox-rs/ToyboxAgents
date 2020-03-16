58dcb8ca6ac284a2a864c31934028b4fafa0cf4b
Emma Tosch <etosch@cs.umass.edu>
2020-03-16 15:18:11 -0400

    adding a logs dir

--- scripts/make_scripts.py
+++ scripts/make_scripts.py
@@ -13,9 +13,10 @@ for agent in ['StayAlive', 'SmarterStayAlive', 'StayAliveJitter', 'Target']:
 
         with open('scripts/run_%s.sh' % agent, 'w') as f:
             content = """#!/bin/bash
+    #
     #SBATCH —job-name={0}
-    #SBATCH --output={0}.out
-    #SBATCH -e {0}.err 
+    #SBATCH --output=logs/{0}_{2}.out
+    #SBATCH -e logs/{0}_{2}.err 
     #SBATCH --nodes=1 
     #SBATCH --ntasks=1 
     #SBATCH --mem=2048
@@ -23,6 +24,7 @@ for agent in ['StayAlive', 'SmarterStayAlive', 'StayAliveJitter', 'Target']:
     source .env/bin/activate
     pip install -r REQUIREMENTS.txt
     mkdir -p {2}/output/{0}/{1}
+    mkdir -p logs
     python -m agents.breakout.stayalive {2}/output/{0} {0}
     """.format(agent, seed, root)
             f.write(content)
