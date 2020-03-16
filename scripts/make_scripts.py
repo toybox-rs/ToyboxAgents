ebc07960e12f9d0bf7d2d706433e4f4936c54ceb
Emma Tosch <etosch@cs.umass.edu>
2020-03-16 15:21:21 -0400

    more small script edits

--- scripts/make_scripts.py
+++ scripts/make_scripts.py
@@ -13,18 +13,18 @@ for agent in ['StayAlive', 'SmarterStayAlive', 'StayAliveJitter', 'Target']:
 
         with open('scripts/run_%s.sh' % agent, 'w') as f:
             content = """#!/bin/bash
-    #
-    #SBATCH —job-name={0}
-    #SBATCH --output=logs/{0}_{2}.out
-    #SBATCH -e logs/{0}_{2}.err 
-    #SBATCH --nodes=1 
-    #SBATCH --ntasks=1 
-    #SBATCH --mem=2048
-
-    source .env/bin/activate
-    pip install -r REQUIREMENTS.txt
-    mkdir -p {2}/output/{0}/{1}
-    mkdir -p logs
-    python -m agents.breakout.stayalive {2}/output/{0} {0}
-    """.format(agent, seed, root)
+#
+#SBATCH —job-name={0}
+#SBATCH --output=logs/{0}_{2}.out
+#SBATCH -e logs/{0}_{2}.err 
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --mem=2048
+source .env/bin/activate
+pip install -r REQUIREMENTS.txt
+mkdir -p {2}/output/{0}/{1}
+mkdir -p logs
+echo "Running {0} with seed {1}..."
+python -m agents.breakout.stayalive {2}/output/{0} {0}
+""".format(agent, seed, root)
             f.write(content)
