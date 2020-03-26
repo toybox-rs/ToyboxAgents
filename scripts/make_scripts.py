c1bea5b96324c0df4ab0895f88e7305d8f74847f
Emma Tosch <etosch@cs.umass.edu>
2020-03-26 11:12:57 -0400

    updates to make_scripts file

--- scripts/make_scripts.py
+++ scripts/make_scripts.py
@@ -10,6 +10,7 @@ root = sys.argv[1] if len(sys.argv) > 1 else '.'
 for agent in ['StayAlive', 'SmarterStayAlive', 'StayAliveJitter', 'Target']:
     # create empty tar file that we will add to in a distributed fashion
     tarfile = root + os.sep + agent + '.tar'
+    echo "Creating empty tar: %s" % tarfile
     subprocess.run(['tar', '-cf', tarfile, '-T', '/dev/null'])
     # run for 30 trials each
     for _ in range(30):
@@ -18,7 +19,7 @@ for agent in ['StayAlive', 'SmarterStayAlive', 'StayAliveJitter', 'Target']:
         with open('scripts/run_%s_%d.sh' % (agent, seed), 'w') as f:
             content = """#!/bin/bash
 #
-#SBATCH —job-name=ToyboxAgents_{0}_{2}
+#SBATCH —job-name=ToyboxAgents_{0}_{1}
 #SBATCH --output=logs/{0}_{2}.out
 #SBATCH -e logs/{0}_{2}.err 
 #SBATCH --nodes=1 
@@ -30,7 +31,6 @@ mkdir -p {2}/output/{0}/{1}
 # We may be -re-running this script; if so, delete old data
 rm {2}/output/{1}/{1}/*
 mkdir -p logs
-echo "Running {0} with seed {1}..."
 python -m agents --game Breakout --output {2}/output/{0} --agentclass {0} --seed {1}
 tar --append --file={3} {2}/output/{0}/*
 """.format(agent, seed, root, tarfile)
