c26c06b07da1abc8b71288c04ed7974afe15a226
Emma Tosch <etosch@cs.umass.edu>
2020-03-31 12:14:07 -0400

    accidentally added code that is not ready to be in here

--- analysis/record.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-class Record(ABC):
-
-    def to_csv_entry(self):
-        dat = sorted(vars(self).items(), key=lambda t: t[0])
-        return ",".join([v for k, v in dat])
-
