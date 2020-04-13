8e4b1663e18126d9f43b7ce743ec7d9cd05fd409
Kenta Takatsu <kt426@cornell.edu>
2020-04-13 19:44:35 -0400

    two new agents

---
+++ agents/breakout/moveonlyfalling.py
@@ -0,0 +1,39 @@
+from . import *
+from random import random, seed
+
+
+class MoveOnlyFalling(Agent):
+    """
+    Try to stay alive when the (1st) ball is moving downward.
+
+    Potential Failure: when the ball bounces in an acute angle, the agent needs to move while the ball is moving upwards
+    """
+
+    def __init__(self, toybox: Toybox):
+        super().__init__(toybox)
+        seed(self.seed)
+        self.ball_moving_down = None
+        self.ball_prevY = None
+
+    def get_action(self):
+        input = Input()
+        input.button1 = True  # push a button so it starts
+
+        with breakout.BreakoutIntervention(self.toybox) as intervention:
+            game = intervention.game
+            bally = game.balls[0].position.y
+
+            if self.ball_prevY is not None:
+                self.ball_moving_down = (bally > self.ball_prevY)
+            self.ball_prevY = bally
+
+            if self.ball_moving_down:
+                # stay alive when the ball is moving down
+                ballx = game.balls[0].position.x
+                paddlex = game.paddle.position.x
+                if ballx < paddlex:
+                    input.left = True
+                elif ballx > paddlex:
+                    input.right = True
+
+        return input
