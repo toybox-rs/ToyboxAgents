904b137f6d1192daee1b8ef26f5464ddde8233de
Kaleigh Clary <kclary@cs.umass.edu>
2021-03-19 17:21:50 -0400

    base amidar agent

--- agents/amidar/__init__.py
+++ agents/amidar/__init__.py
@@ -1,3 +1,13 @@
 from agents.base import Agent
 from ctoybox import Toybox, Input
 import toybox.interventions.amidar as amidar
+
+
+class AmidarAgent(Agent):
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def play(self, *args, **kwargs):
+    #input = Input()
+    super().play(*args, **kwargs)
