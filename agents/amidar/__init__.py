ce284a34314bac0f2894c04ef211978e8b7599d6
Kaleigh Clary <kclary@cs.umass.edu>
2021-04-17 13:44:36 -0400

    abstract syntax

--- agents/amidar/__init__.py
+++ agents/amidar/__init__.py
@@ -1,12 +1,16 @@
+from abc import abstractmethod
+
 from agents.base import Agent
 from ctoybox import Toybox, Input
 import toybox.interventions.amidar as amidar
-
 
 class AmidarAgent(Agent):
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
+
+  @abstractmethod
+  def get_action(self) -> Input: pass
 
   def play(self, *args, **kwargs):
     #input = Input()
